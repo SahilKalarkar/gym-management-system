108ffc99c3873e049167f6e8f3d2a00534e25c66
dhananjay kale <Dhananjaykale877@gmail.com>
2026-03-20 23:51:27 +0530

    ✅ Server mode + Vercel auto-detect

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  eslint: { ignoreDuringBuilds: true }
 }
 module.exports = nextConfig;
